0066f0c0f8f21b8c8ad424dcb5cf4a619893b305
ZiKovski <zikovski@yandex.ru>
2016-06-13 15:14:55 +0300

    new ver

--- include.php
+++ include.php
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 <?= '<a href="#">I need more PHP-code!</a>'?>
-<?= '<a href="#">It is PHP-code.</a>'?>
-=======
-<?= '<a href="#">I need more PHP-code! But I'am lazy.</a>'?>
->>>>>>> include2
